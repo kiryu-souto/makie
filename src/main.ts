52fa0b81de21c03a0c2b077ae1fc9fb339501dbf
kiryu eiji <test@example.com>
2022-12-18 09:34:46 +0900

    save

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ import * as origin_draw from './2_molecules/draws/object_draw'
 import p5 from 'p5'
 
 // object declaration
-let test_text_lst = [new text.Text(new p5.Vector(10, 220), new p5.Vector(20, 230) , "hogehoge")]
+// let test_text_lst = [new text.Text(new p5.Vector(10, 220), new p5.Vector(20, 230) , "hogehoge")]
 let own_rect_lst = [new rect.Rect(new p5.Vector(0, 220), new p5.Vector(20, 30))]
-let test_line_lst = [new line.Line(new p5.Vector(10, 230), new p5.Vector(50, 230))]
+// let test_line_lst = [new line.Line(new p5.Vector(10, 230), new p5.Vector(50, 230))]
 // let test_line_lst = [new line.Line(new p5.Vector(100, 100), new p5.Vector(200, 300)),
 //                     new line.Line(new p5.Vector(0, 300), new p5.Vector(300, 300))]
 let test_rect_lst = [new rect.Rect(new p5.Vector(300, 100), new p5.Vector(30, 300)),
-                     new rect.Rect(new p5.Vector(0, 350), new p5.Vector(200, 30))]
+                     new rect.Rect(new p5.Vector(0, 350), new p5.Vector(300, 30))]
 
 
 const sketch = (p: p5) => {
@@ -24,7 +24,7 @@ const sketch = (p: p5) => {
 
   p.draw = () => {
     p.background(255);
-    if (p.mouseIsPressed) {
+    if (p.keyIsPressed) {
       
       let test = false
       p.fill(0)
@@ -32,20 +32,22 @@ const sketch = (p: p5) => {
       for (let text of own_rect_lst) {
         
         for (let line of test_rect_lst) {
-
           if ( colliders.new_square_collide(text.pos, text.size, line.pos, line.size) instanceof colliders.Inside ) {
             test = true
           }
-          console.log("test_boolean1", colliders.new_square_collide(text.pos, text.size, line.pos, line.size).constructor)
         }
 
         if (test == false) {
-          text.pos.x = text.pos.x + horizontal_movement(10, 1)
+          if (p.keyCode == p.RIGHT_ARROW) {
+            text.pos.x = text.pos.x + horizontal_movement(10, 1)
+          } else if (p.keyCode == p.LEFT_ARROW) {
+            text.pos.x = text.pos.x - horizontal_movement(10, 1)
+          }
         }
 
       }
 
-    } else if (p.keyIsPressed) {
+    } else {
 
       let test = false
 
